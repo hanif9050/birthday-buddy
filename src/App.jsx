19ff23994ad17c56f3c8ca1a6ba5b90b1e2abbe9
Mohammed Hanif <hanif.ctg9050@gmail.com>
2023-12-07 00:21:57 +0600

    css reset

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import data from "./data";
 import List from "./List";
 const App = () => {
   const [people, setPeople] = useState(data);
-  console.log(people);
+
   return (
     <main>
       <section className="container">
         <h3>{people.length} birthdays today</h3>
         <List people={people} />
+        <button
+          type="button"
+          className="btn btn-block"
+          onClick={() => setPeople([])}
+        >
+          Clear All
+        </button>
       </section>
     </main>
   );
